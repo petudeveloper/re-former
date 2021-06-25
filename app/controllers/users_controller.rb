0bff0388dc173c630eefc2b5267daa77e06047e0
petumazo <davidalvarezmazzo@gmail.com>
2021-06-25 12:00:27 -0500

    correct errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-  
+
     if @user.save
       redirect_to new_user_path
     else
@@ -14,12 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    
+
     if @user.update(user_params)
       redirect_to edit_user_path
     else
